1cf1454a1a6dcd045536bf36cc03acde4f590d74
krutovis <ilya.krutov.94@mail.ru>
2023-05-08 18:11:25 +0500

    add brain_even

---
+++ brain_games/calculator.py
@@ -0,0 +1,9 @@
+def check_even(number):
+    if number % 2 == 0:
+        return "yes"
+    else:
+        return "no"
+
+
+if __name__ == "__main__":
+    check_even(22)
---
+++ brain_games/even_answer.py
@@ -0,0 +1,25 @@
+from random import randint
+from brain_games.even_check import check_even
+import prompt
+
+
+def check_answer(name):
+    count = 3
+    while count != 0:
+        number = randint(1, 100)
+        guess = prompt.string('Answer "yes" if the number is even,'
+                              'otherwise answer "no".\n'
+                              f'Question: {number}\n'
+                              'Your answer: ')
+        if guess == check_even(number):
+            count -= 1
+            print("Correct!")
+        else:
+            print("'yes is wrong answer;(. Correct answer was 'no'.\n"
+                  f"Let's try again, {name}!")
+            count = 3
+    print(f"Congratulations, {name}")
+
+
+if __name__ == "__main__":
+    check_answer('Bill')
